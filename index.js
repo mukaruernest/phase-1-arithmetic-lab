9c756b46d32cdadc735e63219110ee5504d3d3d4
mukaruernest <mukaruernesto@gmail.com>
2023-01-11 12:13:03 +0300

    Arithmetic Lab Solutions

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() + 10)
+
+const num3 = 6
+const num4 = 22
+
+const mod = num4 % num3
+
+const set = (2, 4, 3, 20)
+const max = Math.max(set)
